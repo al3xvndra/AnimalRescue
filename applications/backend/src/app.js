570d1d3f7d5e37df8e3631177a7dd620ff1af4b7
Aleksandra <taal21rq@student.ju.se>
2024-09-13 19:23:49 +0200

    Backend connection

--- applications/backend/src/app.js
+++ applications/backend/src/app.js
@@ -1,3 +1,4 @@
+console.log("Hello");
 const express = require("express");
 const mysql = require("mysql2/promise");
 require("dotenv").config({ path: __dirname + "/./../../.env" });
@@ -6,33 +7,12 @@ const app = express();
 
 // Create a connection pool to the database
 const pool = mysql.createPool({
-  host: "localhost",
+  host: "mysql",
   user: "root",
   password: process.env.MYSQL_ROOT_PASSWORD,
   database: "my_database",
   port: 3306,
 });
-
-// async function runQueries() {
-//   try {
-//     // Create a connection from the pool
-//     const connection = await pool.getConnection();
-
-//     try {
-//       // Query using the connection
-//       const results = await connection.query(
-//         "SELECT * FROM `reports` WHERE color=?",
-//         ["white"]
-//       );
-//       console.log(results);
-//     } finally {
-//       connection.release();
-//     }
-//   } catch (err) {
-//     console.log(err);
-//   }
-// }
-// runQueries();
 
 app.get("/reports", async function (request, response) {
   try {
